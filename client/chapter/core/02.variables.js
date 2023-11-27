8677934ee6a60af1776edf290a141bb3fb382b80
zooyaam <yjoo2498@naver.com>
2023-11-27 15:54:14 +0900

    변수

---
+++ client/chapter/core/02.variables.js
@@ -0,0 +1,21 @@
+let name = 'John';
+let admin = name;
+alert(admin);
+
+/* 변수 */
+// 구매 제품 가격의 총 합, 구매 결제 여부, 구매결제 내역, 오늘의 운세
+
+/* 상수 */
+// 1년 기준 일자수, 브랜드 접두사, 상품 정보
+
+// redux, recoil, jotai, zustand = ajax
+
+// panding 아무것도 안함
+// loading 로딩중, 요청 대기
+// fulfilled, resolved 응답 성공
+// rejected 응답 실패
+
+const PANDING = 'api/panding';
+const LOADING = 'api/loading';
+const FULFILLED = 'api/fulfilled';
+const REJECTED = 'api/rejected';
